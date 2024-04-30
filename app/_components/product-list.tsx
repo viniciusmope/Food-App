cdb35c46a0d779a1544f0ee60bbc87b10164fafe
Vinicius Moreira <viniciuspemo1@gmail.com>
2024-04-30 01:36:56 -0300

    refactor: ensure product list receives products as prop

--- app/_components/product-list.tsx
+++ app/_components/product-list.tsx
@@ -1,10 +1,8 @@
 import { Prisma } from "@prisma/client";
-import Image from "next/image";
-import { calculateProductTotalPrice, formatCurrency } from "../_helpers/price";
-import { ArrowDownIcon } from "lucide-react";
+import ProductItem from "./product-item";
 
-interface ProductItemProps {
-  product: Prisma.ProductGetPayload<{
+interface ProductListProps {
+  products: Prisma.ProductGetPayload<{
     include: {
       restaurant: {
         select: {
@@ -12,49 +10,17 @@ interface ProductItemProps {
         };
       };
     };
-  }>;
+  }>[];
 }
 
-const ProductItem = ({ product }: ProductItemProps) => {
+const ProductList = async ({ products }: ProductListProps) => {
   return (
-    <div className="w-[150px] min-w-[150px] space-y-2">
-      <div className="relative h-[150px] w-full">
-        <Image
-          src={product.imageUrl}
-          alt={product.name}
-          fill
-          className="rounded-lg object-cover shadow-md"
-        />
-
-        {product.discountPercentage && (
-          <div className="absolute left-2 top-2 flex items-center gap-[2px] rounded-full bg-primary px-2 py-[2px] text-white">
-            <ArrowDownIcon size={12} />
-            <span className="text-xs font-semibold">
-              {product.discountPercentage}%
-            </span>
-          </div>
-        )}
-      </div>
-
-      <div>
-        <h2 className="truncate text-sm">{product.name}</h2>
-        <div className="flex items-center gap-1">
-          <h3 className="font-semibold">
-            {formatCurrency(calculateProductTotalPrice(product))}
-          </h3>
-          {product.discountPercentage > 0 && (
-            <span className="text-xs text-muted-foreground line-through">
-              {formatCurrency(Number(product.price))}
-            </span>
-          )}
-        </div>
-
-        <span className="block text-xs text-muted-foreground">
-          {product.restaurant.name}
-        </span>
-      </div>
+    <div className="flex gap-4 overflow-x-scroll px-5 [&::-webkit-scrollbar]:hidden">
+      {products.map((product) => (
+        <ProductItem key={product.id} product={product} />
+      ))}
     </div>
   );
 };
 
-export default ProductItem;
+export default ProductList;
